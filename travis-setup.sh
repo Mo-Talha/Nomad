286cfa2cfde75203166acf0cfffaf4703fd71076
Muhammad Talha <talhahilside@gmail.com>
2016-11-12 19:48:54 -0500

    Modify mongodb install on travis

--- travis-setup.sh
+++ travis-setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo service mongodb stop
+
 # Import mongodb public GPG key
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 
@@ -7,3 +9,5 @@ echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" 
 
 sudo apt-get update -qq -y
 sudo apt-get install -y mongodb-org
+
+sudo service mongod start
