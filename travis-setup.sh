5315ad2c71e0fd8f6f73bd091f9f23a5d817e261
Muhammad Talha <talhahilside@gmail.com>
2016-11-12 20:29:03 -0500

    Fix mongod.conf lock on travis

--- travis-setup.sh
+++ travis-setup.sh
@@ -6,9 +6,6 @@ sudo apt-get purge mongodb mongodb-clients mongodb-server mongodb-dev
 sudo apt-get purge mongodb-10gen
 sudo apt-get autoremove
 
-sudo rm -f /etc/mongod.conf
-sudo cp ./config/mongodb_dev.conf /etc/mongod.conf
-
 # Import mongodb public GPG key
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 
@@ -16,5 +13,9 @@ echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" 
 
 sudo apt-get update -qq -y
 sudo apt-get install -y mongodb-org
+
+sudo rm -f /etc/mongod.conf
+
+sudo cp ./config/mongodb_dev.conf /etc/mongod.conf
 
 sudo service mongodb start
