747b8b42c4bfac7f71c3289e2da21ea0122df18c
Muhammad Talha <talhahilside@gmail.com>
2016-11-12 19:52:11 -0500

    Switch to using systemctl for mongodb install in travis

--- travis-setup.sh
+++ travis-setup.sh
@@ -10,4 +10,4 @@ echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" 
 sudo apt-get update -qq -y
 sudo apt-get install -y mongodb-org
 
-sudo service mongod start
+sudo systemctl start mongod
