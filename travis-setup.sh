5f86236349ca3eae9f23d76df754498dd9f6f072
Muhammad Talha <talhahilside@gmail.com>
2016-11-12 19:55:22 -0500

    Switch back to service because of Ubuntu 12

--- travis-setup.sh
+++ travis-setup.sh
@@ -10,4 +10,4 @@ echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" 
 sudo apt-get update -qq -y
 sudo apt-get install -y mongodb-org
 
-sudo systemctl start mongod
+sudo service mongodb start
