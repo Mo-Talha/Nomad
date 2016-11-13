f0e7991044befe54343dfcbe6580b4a903966acd
Muhammad Talha <talhahilside@gmail.com>
2016-11-12 19:24:05 -0500

    Update travis to use mongodb 3.2

---
+++ travis-setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Import mongodb public GPG key
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+sudo apt-get update -qq -y
+sudo apt-get install -y mongodb-org
